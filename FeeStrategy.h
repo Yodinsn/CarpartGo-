7d111456507a282183f9d2b4e7e53faa07181dd2
BNzH <92985237+Yodinsn@users.noreply.github.com>
2025-12-22 12:32:55 +0800

    Create FeeStrategy.h

---
+++ FeeStrategy.h
@@ -0,0 +1,20 @@
+SmartParkingSystem/
+│
+├─ include/
+│   ├─ Vehicle.h
+│   ├─ Car.h
+│   ├─ Motorcycle.h
+│   ├─ FeeStrategy.h
+│   ├─ ParkingSpot.h
+│   ├─ ParkingLot.h
+│   └─ VehicleFactory.h
+│
+├─ src/
+│   ├─ Car.cpp
+│   ├─ Motorcycle.cpp
+│   ├─ ParkingSpot.cpp
+│   ├─ ParkingLot.cpp
+│   └─ VehicleFactory.cpp
+│
+├─ main.cpp
+└─ README.md
